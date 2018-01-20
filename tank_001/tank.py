f8e5cc710ee33c673f19f1ef50e54da6f74c0770
kowaraj <kowaraj4stuff@gmail.com>
2018-01-20 22:50:58 +0100

    last

--- tank_001/tank.py
+++ tank_001/tank.py
@@ -1,16 +1,25 @@
-
 from Tkinter import *
 
 root = Tk()
-
-        
-w = Label(root, text="Hello, im tank!")
+#w = Label(root, text="Hello, im tank!")
 
 
+canvas = Canvas(root, width=500, height=500)
 
 
-w.pack()
 
+x0 = 200
+y0 = 300
+x1 = 500
+y1 = 200
+r = canvas.create_rectangle(x0, y0, x1, y1, fill='blue')
+r = canvas.create_rectangle(100, 100, 200, 400, fill='red')
+w = canvas.create_line(15, 25, 200, 25)
+# dash for _ _ _ _
 
+#dasha for @*#$%@#$%*@#$%
+
+#w.pack()
+canvas.pack()
 root.mainloop()
 
